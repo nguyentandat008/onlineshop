7c7021e393850c5e7e3db35e6725717ed91d1056
DESKTOP-KKB9LVD\Dat <nguyentandat008@gmail.com>
2017-12-05 17:20:05 +0700

    Implement repository

---
+++ OnlineShop.Data/Repositories/OrderDetailRepository.cs
@@ -0,0 +1,16 @@
+﻿using OnlineShop.Data.Infrastructure;
+using OnlineShop.Model.Models;
+
+namespace OnlineShop.Data.Repositories
+{
+    public interface IOrderDetailRepository
+    {
+    }
+
+    public class OrderDetailRepository : RepositoryBase<OrderDetail>, IOrderDetailRepository
+    {
+        public OrderDetailRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ OnlineShop.Data/Repositories/PostCategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using OnlineShop.Data.Infrastructure;
+using OnlineShop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Data.Repositories
+{
+    public interface IPostCategoryRepository
+    {
+    }
+
+    public class PostCategoryRepository : RepositoryBase<PostCategory>, IPostCategoryRepository
+    {
+        public PostCategoryRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}

f4cd0b2883533df0dac5cfa3aeebef7f279c117d
DESKTOP-KKB9LVD\Dat <nguyentandat008@gmail.com>
2017-12-05 16:52:47 +0700

    Migration and repository

--- OnlineShop.Data/OnlineShopDBContext.cs
+++ OnlineShop.Data/OnlineShopDBContext.cs
@@ -1,10 +1,5 @@
 ﻿using OnlineShop.Model.Models;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OnlineShop.Data
 {
